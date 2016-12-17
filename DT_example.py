947446b63075f7ac2a598e3da07610783425d4e7
Xiaodong Wang <xwang322@users.noreply.github.com>
2016-12-17 17:58:54 -0600

    Delete DT_example.py

--- DT_example.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import load_iris
-from sklearn.cross_validation import cross_val_score
-from sklearn.tree import DecisionTreeClassifier
-
-clf = DecisionTreeClassifier(random_state=0)
-iris = load_iris()
-print(iris)
-cross_val_score(clf, iris.data, iris.target, cv=10)
-print(cross_val_score)
